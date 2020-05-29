f2e90f127599f835dedc2536f976302a17a90555
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2020-05-29 18:56:17 +0300

    romoving unnecessary space in a file

--- src/generateRandomSet/GenerateRandomSetProgram.java
+++ src/generateRandomSet/GenerateRandomSetProgram.java
@@ -202,27 +202,6 @@ public class GenerateRandomSetProgram extends JPanel {
 	 */	
 }
 	
-	
-	
-	
-	
-	
-
-
-
-
-
-	
-
-
-
-	
-
-
-
-
-
-
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
